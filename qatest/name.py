60bc28389d342fa44bb3ee004f6a98aae23039c4
Ren <rg326@live.com>
2025-11-02 23:55:14 -0500

    added challenge 2 to name.py

--- qatest/name.py
+++ qatest/name.py
@@ -9,3 +9,10 @@ user_last = input("What is your last name? \n")
 # Prints the full response
 print(f"Your full name is {user_first} {user_last}!")
 
+##
+
+season = input("Enter a season of the year \n")
+num_input = input("Now, enter a number \n")
+adj_input = input("Now, enter an adjective \n")
+
+print(f"On a {adj_input} {season} day, I drink at least {num_input} cups of juice!")
