a7a16ebca169ca9aa3b1280c9f77d8d734e3e0a2
Ren <rg326@live.com>
2025-11-02 23:50:34 -0500

    added pies and qatest dirs, and file name.py

---
+++ qatest/name.py
@@ -0,0 +1,11 @@
+#print("What is your name?")
+
+# Asks for the first name
+user_first = input("What is your first name? \n")
+
+# Asks for the last name
+user_last = input("What is your last name? \n")
+
+# Prints the full response
+print(f"Your full name is {user_first} {user_last}!")
+
