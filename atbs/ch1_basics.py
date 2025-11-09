8d1e74d4edb73f6873c00acf0e9ffb0af5429f6b
Ren <rg326@live.com>
2025-11-08 23:29:44 -0500

    added python basics operations to ch1_basics.py

---
+++ atbs/ch1_basics.py
@@ -0,0 +1,15 @@
+# PYTHON BASICS
+
+# REPL
+print(2 + 2)
+print(2)
+
+# Order of Operations (Precedence)
+print(2 + 3 + 6)
+print((2 + 3) + 6)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(23 % 7)
+print(2 + 2)
+print(((5 - 1) * ((7 + 1) / (3 - 1))))
