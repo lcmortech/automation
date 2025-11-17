5c008f0a9757dafeed8f8a21233528d2fba4a087
Ren <rg326@live.com>
2025-11-16 23:55:14 -0500

    added python keywords to essentials.py

--- pies/essentials.py
+++ pies/essentials.py
@@ -1,4 +1,33 @@
 # KEYWORDS
+False / True
+await / async
+else
+import
+None
+break / continue
+except
+in 
+raise 
+class 
+finally
+is 
+return 
+and
+for 
+lambda 
+try 
+as
+def 
+from 
+nonlocal 
+while 
+assert 
+del 
+global 
+not 
+with 
+async 
+
 
 # LIST METHODS
 
