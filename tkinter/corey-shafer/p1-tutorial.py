e7b6a84329348ae78722a6d908ddebef2d2714bc
Ren <rg326@live.com>
2025-11-20 00:00:54 -0500

    fixed bug in frame variable declaration in p1-tutorial.py

--- tkinter/corey-shafer/p1-tutorial.py
+++ tkinter/corey-shafer/p1-tutorial.py
@@ -4,12 +4,12 @@ root = tk.Tk()
 
 root.title("Simple App")
 
-frame = tk.frame(root)
+frame = tk.Frame(root)
 frame.grid(row=0, column=0)
 
 entry = tk.Entry(frame)
 entry.grid(row=0, column=0)
 
-entry_btn = tk.Button(frame)
+entry_btn = tk.Button(frame, text="Add")
 
 root.mainloop()
