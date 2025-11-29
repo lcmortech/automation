89bb922737f74835658b0f27a5173046fcbdea19
Ren <rg326@live.com>
2025-11-28 23:51:19 -0500

    commented out python keywords in essentials.py

--- pies/essentials.py
+++ pies/essentials.py
@@ -1,35 +1,35 @@
 # KEYWORDS
 False / True
-await / async
-else
-import
-None
-break / continue
-except
-in 
-raise 
-class 
-finally
-is 
-return 
-and
-for 
-lambda 
-try 
-as
-def 
-from 
-nonlocal 
-while 
-assert 
-del 
-global 
-not 
-with 
-async 
-elif / if
-or 
-yield
+# await / async
+# else
+# import
+# None
+# break / continue
+# except
+# in 
+# raise 
+# class 
+# finally
+# is 
+# return 
+# and
+# for 
+# lambda 
+# try 
+# as
+# def 
+# from 
+# nonlocal 
+# while 
+# assert 
+# del 
+# global 
+# not 
+# with 
+# async 
+# elif / if
+# or 
+# yield
 
 
 # LIST METHODS
