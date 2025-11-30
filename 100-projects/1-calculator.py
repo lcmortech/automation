856acf282e6fdde78543b145da70207c842d6cfd
Ren <rg326@live.com>
2025-11-29 23:56:17 -0500

    minor edit to prev

--- 100-projects/1-calculator.py
+++ 100-projects/1-calculator.py
@@ -21,7 +21,7 @@ window.title = "GUI Calculator" #title
 window.geometry("300x275")
 
 # VARIABLES
-text_result = tk.text(window, height=2, width=16, font=("Arial",24))
+text_result = tk.text(window, height=2, width=16, font=("Arial", 24))
 text.grid(row=0,column=1)
 
 window.mainloop() #endpoint
