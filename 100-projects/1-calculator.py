03e64fe2e1f17515e391c52970968c43077853f5
Ren <rg326@live.com>
2025-12-13 23:55:40 -0500

    added equals button to 1-calculator.py

--- 100-projects/1-calculator.py
+++ 100-projects/1-calculator.py
@@ -70,4 +70,6 @@ btn_open = tk.Button(window, text="(", command=lambda: add_to_calculation(1))
 btn_open.grid(row=5, column=3)
 btn_close = tk.Button(window, text=")", command=lambda: add_to_calculation(1)) 
 btn_close.grid(row=5, column=3)
+btn_equals = tk.Button(window, text="=", command=lambda: add_to_calculation(1)) 
+btn_equals.grid(row=5, column=3)
 window.mainloop() #endpoint
