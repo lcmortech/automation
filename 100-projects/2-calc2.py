ee6bc78867888fdb086e82c8b3409669b777e944
Ren <rg326@live.com>
2025-12-25 23:44:56 -0500

    added tkinter import and button values to 2-calc.py

--- 100-projects/2-calc2.py
+++ 100-projects/2-calc2.py
@@ -1,2 +1,12 @@
 # Tkinter calculator build without using eval function
-# Instead, every operation will have its own separate function
+# Instead, every operation will have its own separate function (Kenny Yip)
+
+#setup
+
+import tkinter as tk
+
+#2D list, each list is a row
+button_values = [
+    ["AC", "+/-", "%", "//"],
+    
+]
