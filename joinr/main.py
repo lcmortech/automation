aa1bd29607901edd39e72255fdb5bfc39bf2a03a
Ren <rg326@live.com>
2026-02-16 23:47:07 -0500

    rewrote codebase with pytubefix in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -1,19 +1,30 @@
-from pytube import YouTube
-#from sys import argv
+# from pytube import YouTube
+# #from sys import argv
 
-#from docs
-yt = YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
+# #from docs
+# yt = YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
 
-print("Title:" + yt.title)
-#get resolution
+# print("Title:" + yt.title)
+# #get resolution
 
-print(yt.description)
-desc = yt.description
-length = yt.length
-yt.streams.get_highest_resolution()
-#check link
-yt.download()
+# print(yt.description)
+# desc = yt.description
+# length = yt.length
+# yt.streams.get_highest_resolution()
+# #check link
+# yt.download()
 
-# Bugs:
-# urllib error
-# check api (corey schafer for tips)
+# # Bugs:
+# # urllib error
+# # check api (corey schafer for tips)
+
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+url = "url"
+
+yt = YouTube(url, on_progress_callback=on_progress)
+print(yt.title)
+
+ys = yt.streams.get_highest_resolution()
+ys.download()
