f8a85a5dd14e703c2d956af60a41350a1c59a28a
Ren <rg326@live.com>
2026-02-18 23:56:11 -0500

    added test url to joinr

--- joinr/main.py
+++ joinr/main.py
@@ -21,7 +21,7 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = "url"
+url = "https://www.youtube.com/watch?v=9bZkp7q19f0"
 
 yt = YouTube(url, on_progress_callback=on_progress)
 print(yt.title)
