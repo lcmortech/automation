7dd1252c60aa9261c940bf3073dcc89ed88b94e5
Ren <rg326@live.com>
2026-02-21 23:57:40 -0500

    edited notes on playlists in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -37,4 +37,4 @@ ys.download()
 # fix pytube cli error 
 # https://pytubefix.readthedocs.io/en/latest/
 # 2-18 success!
-# next objective - retrieve playlist (list?)
+# next objective - retrieve playlist (list?) - check!
