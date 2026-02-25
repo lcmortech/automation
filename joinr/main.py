59cc702e1377aa5c63b56a1f51cff6dd0cafff30
Ren <rg326@live.com>
2026-02-24 23:50:31 -0500

    git added dl loop for playlists in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -8,6 +8,8 @@ url = input("Input Youtube Playlist URL")
 # playlist
 pl = Playlist(url)
 
+#download playlist vid loop
 for video in pl.videos:
-    pass
+    ys = video.streams.get_highest_resolution()
+    ys.download()
 
