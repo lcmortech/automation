c406aeaa3d9a6826eff9ae090aac46e0f3d02132
Ren <rg326@live.com>
2026-02-26 23:57:57 -0500

    added notes to prev in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -3,6 +3,7 @@ from pytubefix import YouTube, Playlist
 from pytubefix.cli import on_progress
 
 # get URL (test only short pl, save dir)
+#penpot
 url = input("https://www.youtube.com/playlist?list=PLqazFFzUAPc7g3Yw_0O3LqookKRnnfdZj")
 
 # playlist
