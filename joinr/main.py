21c7643b901635ef415f3a29a960e9071b1df4ee
Ren <rg326@live.com>
2026-02-27 23:57:50 -0500

    added new task list to joinr

--- joinr/main.py
+++ joinr/main.py
@@ -6,6 +6,7 @@ from pytubefix.cli import on_progress
 #penpot
 url = input("https://www.youtube.com/playlist?list=PLqazFFzUAPc7g3Yw_0O3LqookKRnnfdZj")
 
+
 # playlist
 pl = Playlist(url)
 
@@ -14,3 +15,5 @@ for video in pl.videos:
     ys = video.streams.get_highest_resolution()
     ys.download()
 
+#task
+#set new dl dir
