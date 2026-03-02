5aad4fc01e9da6c1b9d7bf62a1d3f2cae34595c9
Ren <rg326@live.com>
2026-03-01 23:45:16 -0500

    changed output path for playlists in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -12,7 +12,7 @@ pl = Playlist(url)
 
 #download playlist vid loop
 for video in pl.videos:
-    ys = video.streams.get_highest_resolution().download(output_path="./yt_playlist")
+    ys = video.streams.get_highest_resolution().download(output_path="/joinr/playlists")
     ys.download()
     print(f"Downloaded: {video.title}")
 
