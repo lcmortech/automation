f786d290988641240bc4dc316eaeb00d02257941
Ren <rg326@live.com>
2026-03-10 22:50:18 -0400

    added merge notes to joinr

--- joinr/main.py
+++ joinr/main.py
@@ -16,6 +16,8 @@ for video in pl.videos:
     ys.download()
     print(f"Downloaded: {video.title}")
 
+clip = VideoFileClip("vid")
+
 #task
 #set new dl dir ()gi
 #increase quality of vid dls (text vid was lq)
@@ -24,3 +26,4 @@ for video in pl.videos:
 # exit .venv
 # pull test - complete
 # moviepy docs (join playlist)
+# add queue variable for clips (if queue then merge with clip at index 0 of queue - while que's index is more than 0?)
