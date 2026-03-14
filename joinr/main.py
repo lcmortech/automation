a801edaaa9ad3ed8e3c97139f9a7536d15092bc0
Ren <rg326@live.com>
2026-03-13 23:59:19 -0400

    code to merge in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -29,6 +29,7 @@ clip = VideoFileClip("directory")
 # exit .venv
 # pull test - complete
 # moviepy docs (join playlist)
+# final_clip = concatenate_videoclips([clip1, clip2, clip3])
 # add queue variable for clips (if queue then merge with clip at index 0 of queue - while que's index is more than 0?)
 # add gui (tkinter)
 #tkinter docs
