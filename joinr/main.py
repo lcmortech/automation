c828a478f7a07afd480fe520d656a0c85753f354
Ren <rg326@live.com>
2026-03-23 23:48:55 -0400

    same as prev in joinr

--- joinr/main.py
+++ joinr/main.py
@@ -40,4 +40,4 @@ clip = VideoFileClip("directory")
 # home recovery - 3/20
 # add ffmpeg to improve dl quality
 # still recovering (anemia nausea)
-# recovering still
+# recovering still (still nausea)
